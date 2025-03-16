3d78235724b9e51ba98d1d13fa6b98db759d2118
Philip Su <39933441+fivecar@users.noreply.github.com>
2025-03-15 21:07:29 -0700

    feat: add forwardRef capabilities (#9)
    This allows callers to get the underlying ScrollView to do things like
    `scrollTo`.

--- src/KeyboardAvoidingScrollView.tsx
+++ src/KeyboardAvoidingScrollView.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {forwardRef, useImperativeHandle} from 'react'
 import {ScrollView, ScrollViewProps} from 'react-native'
 import {
   ExternalKeyboardAvoidingContainerProps,
@@ -10,17 +10,26 @@ export interface KeyboardAvoidingScrollViewProps
   extends ScrollViewProps,
     ExternalKeyboardAvoidingContainerProps {}
 
-export const KeyboardAvoidingScrollView: React.FC<
+export const KeyboardAvoidingScrollView = forwardRef<
+  ScrollView,
   KeyboardAvoidingScrollViewProps
-> = props => {
-  const KeyboardAvoidingContainerProps = useKeyboardAvoidingContainerProps(
-    props,
-  )
+>((props, ref) => {
+  const {
+    scrollViewRef,
+    ...KeyboardAvoidingContainerProps
+  } = useKeyboardAvoidingContainerProps(props)
+
+  // Use useImperativeHandle to expose the internal scrollViewRef to the parent
+  useImperativeHandle(ref, () => {
+    // @ts-expect-error This is how other parts of the codebase use the ref
+    return (scrollViewRef!.current as any)?.getScrollResponder() as ScrollView;
+  })
 
   return (
     <KeyboardAvoidingContainer
       {...KeyboardAvoidingContainerProps}
       ScrollViewComponent={ScrollView}
+      scrollViewRef={scrollViewRef}
     />
   )
-}
+})
